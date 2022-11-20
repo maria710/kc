a93735cd1fd4888d171b0aa2771e244cf0893c4c
maria710 <mariakuruczova7@gmail.com>
2022-11-20 20:33:48 +0100

    added files

---
+++ src/main/resources/static/js/navbar.js
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+fetch('../html/navbar.html')
+  .then(res => res.text())
+  .then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+  })
